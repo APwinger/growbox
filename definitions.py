36c6decc8481a18612b0cc7500e6924900670303
Asher Pollak <apwinger@gmail.com>
2020-02-06 23:54:47 -0500

    added comment describing pinMap

--- definitions.py
+++ definitions.py
@@ -7,4 +7,5 @@ location = '/home/pi/growbox/'
 t = time.localtime() # gives you an actual struct_time object
 h = t.tm_hour
 
+#outlet numbers (ADJ SRP8, 1-8) to BCM pins on RPi 3
 pinMap = { 1:17, 2:23, 3:22, 4:27}
